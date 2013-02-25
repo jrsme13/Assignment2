cf265990239344773b303dbe3115f549f41a7e54
James Spicer <spicerjames1182@hotmail.com>
2013-02-25 22:18:39 +0000

    model class setup

--- Assignment2/Assignment2/Model.cpp
+++ Assignment2/Assignment2/Model.cpp
@@ -1,11 +1,190 @@
 #include "Model.h"
+#include "Direct3D.h"
 
 
-Model::Model(void)
+Model::Model()
+{
+
+	_vertexBuffer = 0;
+	_indexBuffer = 0;
+}
+
+Model::Model(const Model& other)
+{
+
+	
+}
+
+
+Model::~Model()
 {
 }
 
 
-Model::~Model(void)
+bool Model::Initialize(ID3D10Device* device)
 {
+	bool result;
+
+
+	
+	result = InitializeBuffers(device);
+	if(!result)
+	{
+		
+		return false;
+	}
+
+	return true;
+}
+
+
+void Model::Shutdown()
+{
+	
+	ShutdownBuffers();
+
+	return;
+}
+
+void Model::RenderToGraphics(ID3D10Device* device)
+{
+	
+	RenderBuffers(device);
+
+	return;
+}
+
+int Model::GetIndexCount()
+{
+	return _indexCount;
+}
+
+
+bool Model::InitializeBuffers(ID3D10Device* device)
+{
+	Vertex* vertices;
+	unsigned long* indices;
+	D3D10_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
+	D3D10_SUBRESOURCE_DATA vertexData, indexData;
+	HRESULT result;
+
+	_vertexCount = 3;
+	_indexCount = 3;
+
+
+	// Create the vertex array.
+	vertices = new Vertex[_vertexCount];
+	if(!vertices)
+	{
+		return false;
+	}
+
+	// Create the index array.
+	indices = new unsigned long[_indexCount];
+	if(!indices)
+	{
+		return false;
+	}
+
+
+	// Load the vertex array with data.
+	vertices[0].position = D3DXVECTOR3(-1.0f, -1.0f, 0.0f);  // Bottom left.
+	vertices[0].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	vertices[1].position = D3DXVECTOR3(0.0f, 1.0f, 0.0f);  // Top middle.
+	vertices[1].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	vertices[2].position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);  // Bottom right.
+	vertices[2].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	// Load the index array with data.
+	indices[0] = 0;  // Bottom left.
+	indices[1] = 1;  // Top middle.
+	indices[2] = 2;  // Bottom right.
+
+	// Set up the description of the vertex buffer.
+	vertexBufferDesc.Usage = D3D10_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = sizeof(VertexType) * m_vertexCount;
+	vertexBufferDesc.BindFlags = D3D10_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+
+	// Give the subresource structure a pointer to the vertex data.
+	vertexData.pSysMem = vertices;
+
+	// Now finally create the vertex buffer.
+	result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &m_vertexBuffer);
+	if(FAILED(result))
+	{
+		return false;
+	}
+
+	// Set up the description of the index buffer.
+	indexBufferDesc.Usage = D3D10_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(unsigned long) * m_indexCount;
+	indexBufferDesc.BindFlags = D3D10_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+
+	// Give the subresource structure a pointer to the index data.
+	indexData.pSysMem = indices;
+
+	// Create the index buffer.
+	result = device->CreateBuffer(&indexBufferDesc, &indexData, &m_indexBuffer);
+	if(FAILED(result))
+	{
+		return false;
+	}
+
+	// Release the arrays now that the vertex and index buffers have been created and loaded.
+	delete [] vertices;
+	vertices = 0;
+
+	delete [] indices;
+	indices = 0;
+
+	return true;
+}
+
+
+void Model::ShutdownBuffers()
+{
+	// Release the index buffer.
+	if(_indexBuffer)
+	{
+		_indexBuffer->Release();
+		_indexBuffer = 0;
+	}
+
+	// Release the vertex buffer.
+	if(_vertexBuffer)
+	{
+		_vertexBuffer->Release();
+		_vertexBuffer = 0;
+	}
+
+	return;
+}
+
+
+void Model::RenderBuffers(ID3D10Device* device)
+{
+	unsigned int stride;
+	unsigned int offset;
+
+
+	// Set vertex buffer stride and offset.
+	stride = sizeof(Vertex); 
+	offset = 0;
+    
+	// Set the vertex buffer to active in the input assembler so it can be rendered.
+	device->IASetVertexBuffers(0, 1, &_vertexBuffer, &stride, &offset);
+
+	// Set the index buffer to active in the input assembler so it can be rendered.
+	device->IASetIndexBuffer(_indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set the type of primitive that should be rendered from this vertex buffer, in this case triangles.
+	device->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	return;
 }
--- Assignment2/Assignment2/Model.h
+++ Assignment2/Assignment2/Model.h
@@ -11,7 +11,32 @@ class Model
 {
 public:
 	Model();
+	Model(const Model&);
 	~Model();
+
+	bool Initialize(ID3D10Device*);
+	void Shutdown();
+	void RenderToGraphics(ID3D10Device*);
+
+	int GetIndexCount();
+
+
+private:
+
+
+	struct Vertex
+	{
+		D3DXVECTOR3 position;
+		D3DXVECTOR4 color;
+	};
+
+	bool InitializeBuffers(ID3D10Device*);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D10Device*);
+
+	ID3D10Buffer *_vertexBuffer, *_indexBuffer;
+	int _vertexCount, _indexCount;
+
 };
 
 #endif
