13d065ba47ac928052600d37d86dc8afbbfdcdd7
James <jrsme13@hotmail.co.uk>
2013-03-05 17:05:03 +0000

    day uni

--- Assignment2/Assignment2/Direct3D.h
+++ Assignment2/Assignment2/Direct3D.h
@@ -6,8 +6,8 @@
 
 //linking to librarys
 
-#pragma comment(lib,"d3d10.lib")
-#pragma comment(lib,"d3dx10.lib")
+#pragma comment(lib,"C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3d10.lib")
+#pragma comment(lib,"C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3dx10.lib")
 #pragma comment(lib,"dxgi.lib")
 
 
